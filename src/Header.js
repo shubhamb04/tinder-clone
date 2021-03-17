befc9fa3381ed18a62e3cf26e1d36ffdaa769267
Shubham Bhavsar <shubhambhavsar50@gmail.com>
2021-03-17 12:12:48 +1100

    Finished Header component

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PersonIcon from '@material-ui/icons/Person';
 import IconButton from '@material-ui/core/IconButton';
 import TinderLogo from "../src/images/tinderlogo.JPG"
+import ForumIcon from "@material-ui/icons/Forum"
 
 function Header(props) {
     return (
@@ -11,6 +12,9 @@ function Header(props) {
             <PersonIcon fontSize="large" className="header__icon"/>
             </IconButton>
             <img className="header__logo" src={TinderLogo} alt="tinderLogo"/>
+            <IconButton>
+            <ForumIcon fontSize="large" className="header__icon"/> 
+            </IconButton>
         </div>
     );
 }
